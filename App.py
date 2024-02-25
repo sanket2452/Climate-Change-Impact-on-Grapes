ba4d7f50d1864846c7afcc3528479648ac3c9c0e
Pranav Khaire <pranavkhaire28@gmail.com>
2024-02-25 19:01:39 +0530

    Decision Tree

--- App.py
+++ App.py
@@ -1,39 +1,37 @@
 import streamlit as st
+import joblib
+import pandas as pd
 
 def main():
+    # Load the trained model
+    tree_model = joblib.load('tree_model.pkl')
+
     # Set page title
     st.title("Climate Effect On Grapes")
 
-    # Create two columns for input fields
-    col1, col2 ,col3 = st.columns(3)
+    # Input fields for each parameter
+    Temp = st.number_input("Air Temperature ", value=0.0)
+    relativeHumidity = st.number_input("Relative Humidity  ", value=0.0)
+    rain = st.number_input("Enter Rain", value=0.0)
+    windSpeed = st.number_input("Enter Wind Speed ", value=0.0)
+    leafWetnessHour = st.number_input("Enter leaf Wetness/hr", value=0.0)
 
-    # Input fields for each parameter in the first column
-    with col1:
-        months = st.number_input("Enter months", min_value=1, max_value=12, value=1)
-        airtemp_mean = st.number_input("Enter Airtemp mean", value=0.0)
-        rel_hum_mean = st.number_input("Enter Rel.Hum mean", value=0.0)
+    # Button to make predictions
+    if st.button("Predict"):
+        # Create a DataFrame with the user input
+        user_input = pd.DataFrame({
+            'Airtemp': [Temp],
+            'Rel.Hum': [relativeHumidity],
+            'Rain': [rain],
+            'W.Speed': [windSpeed],
+            'leaf wetness hr.': [leafWetnessHour]
+        })
 
-    # Input fields for each parameter in the second column
-    with col2:
-        rain = st.number_input("Enter Rain", value=0.0)
-        radiation_mean = st.number_input("Enter Radiation mean", value=0.0)
-        w_speed_mean = st.number_input("Enter W.Speed mean", value=0.0)
-    
-    with col3:
-        day_length_hrs = st.number_input("Enter Day length hrs.", value=0.0)
-        leaf_wetness_hr = st.number_input("Enter leaf wetness hr.", value=0.0)
+        # Use the loaded model to make predictions
+        prediction = tree_model.predict(user_input)
 
-
-    # Button to display input values
-    if st.button("Print Input Values"):
-        st.write("Months:", months)
-        st.write("Airtemp mean:", airtemp_mean)
-        st.write("Rel.Hum mean:", rel_hum_mean)
-        st.write("Rain:", rain)
-        st.write("Radiation mean:", radiation_mean)
-        st.write("W.Speed mean:", w_speed_mean)
-        st.write("Day length hrs.:", day_length_hrs)
-        st.write("Leaf wetness hr.:", leaf_wetness_hr)
+        # Display the predicted value to the user
+        st.write("Predicted value:", prediction)
 
 if __name__ == "__main__":
     main()
